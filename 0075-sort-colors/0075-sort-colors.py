d57b49d2050d788974e9e4420ca981676f2257b1
Furqan <62575607+MFurqanHashmi@users.noreply.github.com>
2024-06-15 18:26:58 -0400

    Time: 33 ms (82.25%), Space: 16.6 MB (20.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -12,8 +12,28 @@ class Solution:
         - Sort in-place (can't save data elsewhere)
         """
 
-        for i in range(len(nums)):
-            for j in range(i, len(nums)):
-                if nums[i] > nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i] # swap
-        
+        # # Brute Force solution - Selection Sort [O(n^2)]
+        # for i in range(len(nums)):
+        #     for j in range(i, len(nums)):
+        #         if nums[i] > nums[j]:
+        #             nums[i], nums[j] = nums[j], nums[i] # swap
+
+        # Optimized solution - Pointers [O(n)]
+        """
+        This strategy works by using three pointers to partition the array into 
+        sections of 0s, 1s, and 2s in a single pass, swapping elements to place 
+        0s at the beginning and 2s at the end, achieving sorting in constant space 
+        and linear time.
+        """
+        red, white, blue = 0, 0, len(nums) - 1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
